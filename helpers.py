79c6f1016b912b69f63fa9833cbd17a2227d2463
christot20 <thomaschristo1234@yahoo.com>
2022-03-21 21:35:10 -0400

    added some more stuff, works

---
+++ helpers.py
@@ -0,0 +1,6 @@
+state_list = ['', 'Tel-Aviv', 'New York', 'London', 'Madrid', 'Alliance',
+             'Mocoa', 'March', 'San Miguel', 'Neiva', 'Naranjito', 'San Fernando',
+             'Alliance', 'Progreso', 'NewYork', 'Toronto']
+def checker(city, state): #check if state in state list, then check if city in state and return true if true or false if not
+    
+    return False
--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver 
+from helpers import checker
 
 PATH = "D:\Indeed Scraper\chromedriver.exe"
 
 
 if __name__ == '__main__':
     #have input to replace city and job title as well as replace spaces with +, look back at that re thing that guy made for jeffreys equation 
-    city = input("Enter name of city to conduct search (E.g. New York City): ")  #make the input all caps
-    state = input("Enter abbreviation of state to conduct search (E.g. NY): ")
-    job = input("Enter name of job to search up (E.g. Data Engineer): ")
-    city.replace(" ", "+")
-    job.replace(" ", "+")
+    #     
+    city = input("Enter name of city to conduct search (E.g. New York City): ").replace(" ", "+")  
+    state = input("Enter abbreviation of state to conduct search (E.g. NY): ").strip(" ")
+    job = input("Enter name of job to search up (E.g. Data Engineer): ").replace(" ", "+")
 
-    #WebDriverWait wait = new WebDriverWait(driver, 100)
-    if job != "" and city != "" and state != "":
+    city = city.strip(" ") #remove whitespace
+    job = job.strip(" ")
+
+
+    #checker(city, state) #implement this and import it here
+    #if this return true, go through with the search
+    
+    #maybe use geography thing to see if place actually exists?
+    #start with america and move on from there
+
+    if len(state) != 2:
+        #quit out of application/raise error
+        raise ValueError("Invalid State Received.")
+    #elif checker(city, state) == False:
+
+    #else:
+    elif job != "" and city != "" and state != "": #checker comes out false, raise value error as not real city, else do this
         driver = webdriver.Chrome(PATH)
         driver.get(f"https://www.indeed.com/jobs?q={job.upper()}&l={city.upper()}%2C+{state.upper()}&radius=100") #example using ny
         print(driver.title) #prints out total number of jobs with that title in 100 mile radius of that city
