6a86c10a2738232b1a1aa979661a45d80cf77107
thomaschristo1234@yahoo.com <thomaschristo1234@yahoo.com>
2022-03-23 20:52:33 -0400

    sql db added for geo

--- helpers.py
+++ helpers.py
@@ -1,29 +1,39 @@
-from geopy.geocoders import Nominatim
-state_list = ['AL', 'AK', 'AZ', 'AR', 'CA','CO', 
-             'CT', 'DE', 'DC', 'FL', 'GA','HI', 
-             'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 
-             'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 
-             'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 
-             'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 
-             'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 
-             'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 
-             'WV', 'WI', 'WY']
-geolocator = Nominatim(user_agent="indeedscraper")
+#from geopy.geocoders import Nominatim
+import sqlite3
+# state_list = ['AL', 'AK', 'AZ', 'AR', 'CA','CO', 
+#              'CT', 'DE', 'DC', 'FL', 'GA','HI', 
+#              'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 
+#              'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 
+#              'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 
+#              'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 
+#              'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 
+#              'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 
+#              'WV', 'WI', 'WY']
+#geolocator = Nominatim(user_agent="indeedscraper")
 
 def checker(city, state): #check if state in state list, then check if city in state and return true if true or false if not
-        if state not in state_list:
-            return False
-        elif geocoder.google(f"{city}, {state}") == "<[REQUEST_DENIED] Google - Geocode [empty]>":     #over here, might be able to get rid of top thing too, implement sql grabbing
-            return False
-        else:
-            return True
-    #return False
+    sqliteConnection = sqlite3.connect('locations.db')
+    cursor = sqliteConnection.cursor()
+    print(city)
+    list = cursor.execute("SELECT zip FROM mytable WHERE state = ? AND primary_city = ?;", (state,city)).fetchone()
+    print(list)
+    sqliteConnection.close()
+    if list != None:
+        # sqliteConnection.close()
+        return True
+    # else:
+    #     sqliteConnection.close()
+    #     return False
+    return False
 
-me = "Seattle"
-you = "NJ"
-#print(f'{me}, {you}')
-print(geolocator.geocode('Seattle, WA'))
-print(geolocator.geocode(f'{me}, {you}'))
+def fixer(text):
+    for i in range(len(text)):
+        if i == 0:
+            text[i].upper()
+        elif text[i] == " ":
+            text[i+1].upper()
+        i += 1
+    return text
 
 #new plan, just look through sql database :)
 #do it where geocoder thing is
--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver 
-from helpers import checker
+from helpers import checker, fixer
 
-PATH = "D:\Indeed Scraper\chromedriver.exe"
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 
 if __name__ == '__main__':
     #have input to replace city and job title as well as replace spaces with +, look back at that re thing that guy made for jeffreys equation 
     #     
-    city = input("Enter name of city to conduct search (E.g. New York City): ").replace(" ", "+")  
-    state = input("Enter abbreviation of state to conduct search (E.g. NY): ").strip(" ")
-    job = input("Enter name of job to search up (E.g. Data Engineer): ").replace(" ", "+")
+    city = input("Enter name of city to conduct search (E.g. New York): ")
+    city = fixer(city)
+    state = input("Enter abbreviation of state to conduct search (E.g. NY): ").upper()
+    job = input("Enter name of job to search up (E.g. Data Engineer): ").upper()
 
-    city = city.strip(" ") #remove whitespace
-    job = job.strip(" ")
-
-
-    #checker(city, state) #implement this and import it here
-    #if this return true, go through with the search
-    
-    #maybe use geography thing to see if place actually exists?
-    #start with america and move on from there
-
-    if len(state) != 2:
+    if len(state.strip()) != 2:
         #quit out of application/raise error
         raise ValueError("Invalid State Received")
-    elif checker(city.upper(), state.upper()) == False:
+    elif checker(city, state.strip()) == False:
         raise ValueError("Location Received Invalid")
     #else:
     else: #checker comes out false, raise value error as not real city, else do this
+        city = city.replace(" ", "+")
+        job = job.replace(" ", "+")
         driver = webdriver.Chrome(PATH)
         driver.get(f"https://www.indeed.com/jobs?q={job.upper()}&l={city.upper()}%2C+{state.upper()}&radius=100") #example using ny
         print(driver.title) #prints out total number of jobs with that title in 100 mile radius of that city
