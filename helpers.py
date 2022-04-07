6df2262d234a5fd057291676aebaedb2f388c27b
thomaschristo1234@yahoo.com <thomaschristo1234@yahoo.com>
2022-04-07 18:15:33 -0400

    added salary stuff and some keywords, will change graphs

--- helpers.py
+++ helpers.py
@@ -2,9 +2,124 @@
 
 import sqlite3
 
-data_jobs = ["Data Scientist", "Data Engineer", "Data Analyst", "Data Architect", "Machine Learning Engineer",
-            "Business Intelligence Analyst", "Business Analyst", "Statistician", "Quantitative Analyst",
-            "Operations Analyst", "Marketing Analyst", "Database Administrator"]
+data_jobs = ["Data Scientist", "Data Engineer", "Data Analyst", "Data Architect", "Machine Learning Engineer"]
+
+key_words = ["python","sql","mysql","postgresql", "nosql","mongodb","tableau","excel","ai","artificial intelligence","ml","machine learning",
+            "r","matlab","data visualization","data cleaning","linear algebra","calculus","statistics","pytorch",
+            "tensorflow","jupyter notebook","pandas","numpy","matplotlib","seaborn","scipy","spreadsheets","writing",
+            "speaking","communication","neural net","neural network", "neural networks","models","modelling","algorithms",
+            "programming","mathematics","ci/cd","continuous integration","continuous deployment", "continuous delivery",
+            "bash","powershell","windows","linux","unix","git","version control","deep learning","automation","data wrangling",
+            "big data","processing","c","c++","c/c++","perl","java","sas","hadoop","spark","hive","pig","probability",
+            "data manipulation","julia","machine learning algorithms","supervised learning","unsupervised learning",
+            "semi-supervised learning","reinforcement learning","linear regression","logistic regression","decision tree",
+            "svm algorithm","naive bayes algorithm","knn algorithm","knn","svm","support vector machine","naive bayes","k-means",
+            "random forest","random forest algorithm","dimensionality reduction","gradient boosting","adaboosting",
+            "sklearn","scikit-learn","scala","hpcc","storm","cloudera","rapidminer","spss","docker","kubernetes","mapreduce",
+            "cassandra","data transformation","hevo data","matillion","etl","pentaho data integration","talend","infosphere datastage",
+            "data ingestion","apache","data warehousing","aws","aws glue","stitch","data buffering","kinesis","redis cache",
+            "redis","GCP","azure","openstack","openshift","qlik","tibco spotfire","plotly","coding","golang","hbase","sql databases",
+            "sql database","scripting","dbms","data models","security","performance","scalability","data recovery","reliabilty",
+            "migration","data migration","monitor","management systems","relational database","cloud","mining","data mining",
+            "predictive modeling","natrual language processing","nltk","nlp","agile","data structures","hyposthesis testing",
+            "optimization","predictive models","kafka","weka","apiori","apiori algorithm"]
+
+def sal_assigner_less(sal):
+    value = ""
+    for j in range(len(sal)):
+        if sal[j] == ".":
+            value += sal[j]
+        elif sal[j].isdigit() == True:
+            value += sal[j]
+    return value
+
+def sal_assigner(sal):
+    low_val = ""
+    high_val = ""
+    k = 0
+    for j in range(len(sal)):
+        if sal[j] == "$":
+            k += 1
+        if k == 1:
+            if sal[j] == ".":
+                low_val += sal[j]
+            elif sal[j].isdigit() == True:
+                low_val += sal[j]
+        elif k == 2:
+            if sal[j] == ".":
+                high_val += sal[j]           # test out and see if decimal hourly pay is working (like 26.98)
+            elif sal[j].isdigit() == True:   #decimals work, try to make graphs look better, implement sql db
+                high_val += sal[j]           #make new sql db and add data into there
+    return low_val, high_val                 #job_name, job_state, job_city, job_salary_high, job_salary_low, keywords
+                                             #create a list of keywords and implement the NLTK thing to count them, used to find skills
+#ranges: <50K, 50K, 75K, 100K, 125K, 150K, 200K>
+def sal_range(value):
+    #check range of value here
+    if value < 50000:
+        return "50K-"
+    elif 50000 <= value < 75000:
+        return "50K+"
+    elif 75000 <= value < 100000:
+        return "75K+"
+    elif 100000 <= value < 125000:
+        return "100K+"
+    elif 125000 <= value < 150000:
+        return "125K+"
+    elif 150000 <= value < 200000:
+        return "150K+"
+    elif 200000 <= value:
+        return "200K+"
+
+def sal_range_month(value):
+    if value < 4167:
+        return "50K-"
+    elif 4167 <= value < 6250:
+        return "50K+"
+    elif 6250 <= value < 8333:
+        return "75K+"
+    elif 8333 <= value < 10417:
+        return "100K+"
+    elif 10417 <= value < 12500:
+        return "125K+"
+    elif 12500 <= value < 16667:     # add a function for months and check if the by hour numbers look good (no period was found b4)
+        return "150K+"
+    elif 16667 <= value:
+        return "200K+"
+
+def hour_range(value):
+    #check range of value here
+    if value < 26:
+        return "50K-"
+    elif 26 <= value < 39:
+        return "50K+"
+    elif 39 <= value < 51:
+        return "75K+"
+    elif 51 <= value < 64:
+        return "100K+"
+    elif 64 <= value < 77:
+        return "125K+"           #fix how decimals are handled and check what the number is on each func call
+    elif 77 <= value < 103:     # add a function for months and check if the by hour numbers look good (no period was found b4)
+        return "150K+"
+    elif 103 <= value:
+        return "200K+"
+
+def sal_range_est(value):
+    #check range of value here
+    if value < 50:
+        return "50K-"
+    elif 50 <= value < 75:
+        return "50K+"
+    elif 75 <= value < 100:
+        return "75K+"
+    elif 100 <= value < 125:
+        return "100K+"
+    elif 125 <= value < 150:
+        return "125K+"
+    elif 150 <= value < 200:
+        return "150K+"
+    elif 200 <= value:
+        return "200K+"
+
 def checker(city, state): # check if state in state list, then check if city in state and return true if true or false if not
     sqliteConnection = sqlite3.connect('locations.db') # connect to db
     cursor = sqliteConnection.cursor() # cursor for query execution
--- main.py
+++ main.py
@@ -16,17 +16,28 @@ import seaborn as sns
 
 
 
-from helpers import checker, scrape_amount, data_jobs
+from helpers import checker, sal_assigner, scrape_amount, sal_range, hour_range, sal_range_est, sal_range_month, sal_assigner_less, data_jobs, key_words
 
 #PATH = "D:\Indeed-Scraper\chromedriver.exe"
 words = {}
-salaries = {}
+freq_key_words = {}
+high_salaries = {}
+low_salaries = {}
 nltk.download('stopwords')
 
-def plotter(dict):
+def plotter(dict):                   # look to see if more values are needed in the keywords
+    #%matplotlib inline             # sort the graphs to be highest first and go down, also title the plots
+    ranges = list(dict.keys())
+    values = list(dict.values())
+
+    plt.bar(range(len(dict)), values, tick_label=ranges)
+    plt.show()
+
+def word_plotter(dict):
     #%matplotlib inline
-    sns.set()
+    sns.set_theme()
     freqdist1 = nltk.FreqDist(dict)
+    #plt.xticks(rotation=60)
     freqdist1.plot(25)                # have the plot give the name of the job and place its checking as its title
     #return False
 
@@ -42,47 +53,108 @@ def extract(text):
     for new_word in word:
         if new_word not in stop_words:
             if re.search('[a-zA-Z]', new_word) == None:
-                continue #try to maybe limit it to tech jobs?, get list of tech job names and force that to be used
-                # add data to a sql db by job name, city, state, word, and frequency
-                # use db for tableau visualizations
-                # maybe just get salaries?, would be easier, for now...
-                # list of tech skills, porgramming languages, oses, maybe just count those?
-                # list of data skills, their salaries and positions, and where they are
-                # big data job stuff, make new repo with that, keep this for later
-                #make list of jobs to search throug in helpers
+                continue 
             elif not new_word in words:
                 words[new_word.lower()] = 1
                 # try to make a pandas data frame from the dict with key being word and val being its count
             else:
                 words[new_word.lower()] += 1
         else:
-            continue
-    #print(words)
-    #time.sleep(10)
-    #return words                       
+            continue 
+    for kwords in word:
+        if kwords.lower() in key_words:
+            if not kwords in freq_key_words:
+                freq_key_words[kwords.upper()] = 1
+            else:
+                freq_key_words[kwords.upper()] += 1
+def sal_dict(salary, sdict):
+    if not salary in sdict:
+        sdict[salary] = 1
+            # try to make a pandas data frame from the dict with key being word and val being its count
+    else:
+        sdict[salary] += 1
+    #print(sdict) 
+
+def sal_extract(salary):
+    print(salary)
+    z = 0
+    for i in range(len(salary)):
+        if salary[i] == "$":
+            z += 1
+        #return i    #check how many values there are  
+    print(z)    
+    if "year" in salary:      #salary scraping
+        salary.replace(",","")  #remove commas
+        if z > 1:
+            low_val, high_val = sal_assigner(salary)
+            if "Estimated" in salary:
+                new_salary_high = sal_range_est(float(high_val)) # will go through these functions to get salary range
+                new_salary_low = sal_range_est(float(low_val))
+            else:
+                new_salary_high = sal_range(float(high_val)) # will go through these functions to get salary range
+                new_salary_low = sal_range(float(low_val))
+            #try to find if its an horuly or salary value, if there is one or two of them, new salary value range based on number
+            #will need to make the values into numbers at some point, need to work on that
+            #make a fucntion in helpers to determine what range value is in
+            #one fucntion for hourly and one for salaries
+            sal_dict(new_salary_high, high_salaries)   # add new salary ranges to dict
+            sal_dict(new_salary_low, low_salaries)  # will want to make the processes for if i > 1 or not into functions
+                                                    # you also need to split this up with if estimated is in the string or not (diff vals)
+        else: #if only 1 val
+            value = sal_assigner_less(salary)
+            new_salary_high = sal_range(float(value)) # will go through these functions to get salary range
+            new_salary_low = sal_range(float(value))
+            sal_dict(new_salary_high, high_salaries)
+            sal_dict(new_salary_low, low_salaries)
+    elif "month" in salary:      #salary scraping
+        salary.replace(",","")  #remove commas
+        if z > 1:
+            low_val, high_val = sal_assigner(salary)
+            new_salary_high = sal_range_month(float(high_val)) # will go through these functions to get salary range
+            new_salary_low = sal_range_month(float(low_val))
+            sal_dict(new_salary_high, high_salaries)   # add new salary ranges to dict
+            sal_dict(new_salary_low, low_salaries)  # will want to make the processes for if i > 1 or not into functions
+                                                    # you also need to split this up with if estimated is in the string or not (diff vals)
+        else: #if only 1 val
+            value = sal_assigner_less(salary)
+            new_salary_high = sal_range_month(float(value)) # will go through these functions to get salary range
+            new_salary_low = sal_range_month(float(value))
+            sal_dict(new_salary_high, high_salaries)
+            sal_dict(new_salary_low, low_salaries)
+    elif "hour" in salary: #if hour in string
+        if z > 1: #will need seaparate function to determine hourly pay to yearly
+            low_val, high_val = sal_assigner(salary) #might want to make the processes above a function for getting th values
+            new_salary_high = hour_range(float(high_val)) # will go through these functions to get salary range
+            new_salary_low = hour_range(float(low_val))
+            sal_dict(new_salary_high, high_salaries)
+            sal_dict(new_salary_low, low_salaries)
+        else: #if only 1 val
+            value = sal_assigner_less(salary)
+            new_salary_high = hour_range(float(value)) # will go through these functions to get salary range
+            new_salary_low = hour_range(float(value))
+            sal_dict(new_salary_high, high_salaries)
+            sal_dict(new_salary_low, low_salaries)
+    print(high_salaries)
+    print("--------------------------------")
+    print(low_salaries)
+
 def sal_scrape(element):
     if element.find_elements(By.XPATH, './/div[@class="metadata salary-snippet-container"]') == []:
         if element.find_elements(By.XPATH, './/span[@class="estimated-salary"]') == []:
             return False
         else:
             salary = element.find_elements(By.XPATH, './/span[@class="estimated-salary"]')[0].text
+            #print(salary)
             sal_extract(salary)
     else:
         salary = element.find_elements(By.XPATH, './/div[@class="metadata salary-snippet-container"]')[0].text
+        #print(salary)
         sal_extract(salary)
-
-def sal_extract(salary):
-    if not salary in salaries:
-        salaries[salary] = 1
-            # try to make a pandas data frame from the dict with key being word and val being its count
-    else:
-        salaries[salary] += 1
-    print(salaries)
 
 def scrape(num):
     for i in range(num):
         try: 
-            WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, '//div[@id="popover-x"]/button[@aria-label="Close"]')))
+            WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.XPATH, '//div[@id="popover-x"]/button[@aria-label="Close"]')))
             driver.find_element(By.XPATH, '//div[@id="popover-x"]/button[@aria-label="Close"]').click()
         except WebDriverException:
             continue
@@ -90,7 +162,8 @@ def scrape(num):
             for tag in driver.find_elements(By.XPATH, '//div[@id="mosaic-provider-jobcards"]/a'): #iterates over every link
                 #tag.click()
                 #time.sleep(2) 
-                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((tag)))
+                WebDriverWait(driver, 10).until(EC.visibility_of((tag)))
+                WebDriverWait(driver, 5).until(EC.element_to_be_clickable((tag)))
                 try:
                     tag.click()
                     element = driver.find_element(By.XPATH, '//div[@id="mosaic-provider-jobcards"]//section[@id="vjs-container"]/iframe[@title="Selected Job Details"]') #maybe make this elements instead of element and                                                                                                                 #do what u did for checking if there is next button (if cnditional)
@@ -99,6 +172,8 @@ def scrape(num):
                     soup = BeautifulSoup(html, "html.parser")
                     title = soup.find("div", {"class":"jobsearch-JobInfoHeader-title-container"})
                     result = soup.find("div", {"id":"jobDescriptionText"})
+                    print("--------------------------------")
+                    print(title.text)
                     print("--------------------------------")
                     sal_scrape(tag)
                     extract(result.text)
@@ -116,6 +191,7 @@ def scrape(num):
                 print("hi")
                 WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, '//nav[@role="navigation"]//a[@aria-label="Next"]')))
                 driver.find_element(By.XPATH, '//nav[@role="navigation"]//a[@aria-label="Next"]').click()
+                i += 1
             except WebDriverException:
                 print("bye")
                 break          
@@ -124,8 +200,10 @@ def scrape(num):
         # else:
         #     next_pg[0].click() #check if next button exists and click it if yes, else break the loop
         #     i += 1
-    plotter(words)
-    plotter(salaries)
+    word_plotter(words)
+    word_plotter(freq_key_words)
+    plotter(high_salaries)
+    plotter(low_salaries)
     #time.sleep(20)
     driver.quit()
 
